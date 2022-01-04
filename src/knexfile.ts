d22aa61b1f9cdb3ab46296f606836f81bead272e
Leonardo Ramos <leorrc96@gmail.com>
2022-01-04 19:46:32 -0300

    set production config for database

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -20,21 +20,22 @@ const config: knexConfig = {
     }
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+      extension: 'ts',
+      tableName: 'knex_migrations',
+    }
+  }
 }
 
 export default config;
